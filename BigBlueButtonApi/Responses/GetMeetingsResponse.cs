62a4c151a6b4f3ec8124ef1a0c95030670ca5b44
mesut.cakir@hotmail.com.tr <mesut.murat.cakir@hotmail.com>
2020-04-07 15:22:13 +0300

    getAllMeetings eklendi.

---
+++ BigBlueButtonApi/Responses/GetMeetingsResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+
+namespace BigBlueButtonApi.Responses
+{
+    [Serializable]
+    [DesignerCategory("code")]
+    [XmlType(AnonymousType = true)]
+    [XmlRoot(Namespace = "", IsNullable = false)]
+    public class GetMeetingResponse : Response
+    {
+        public Meetings Meetings { get; set; }
+    }
+    [Serializable]
+    public class Meetings
+    {
+        public List<Meeting> Meeting { get; set; }
+    }
+    [Serializable]
+    public class Meeting
+    {
+        public string MeetingID { get; set; }
+
+        public string MeetingName { get; set; }
+
+        public long CreateTime { get; set; }
+
+        public string AttendeePW { get; set; }
+
+        public string ModeratorPW { get; set; }
+
+        public bool Running { get; set; }
+
+        public bool HasBeenForciblyEnded { get; set; }
+    }
+}
